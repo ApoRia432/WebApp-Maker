6ca2e1e39619b220b122a89814340f5f88173b68
ApoRia432 <lecterkn@proton.me>
2025-12-30 08:25:53 +0900

    refactor: utils

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,5 @@
-import { desktopFile, getFavicon } from "./utils";
+import { desktopFile, getFavicon, prompt, writeImageFile } from "./utils";
 import { mkdir } from "fs/promises";
-
-const prompt = async (message: string, example?: string) => {
-  process.stdout.write(`${message}\n`);
-  if (example) {
-    process.stdout.write(`(${example})\n`);
-  }
-  process.stdout.write('-> ');
-  for await (const line of console) {
-    return line;
-  }
-  return;
-}
-
-const writeImageFile = async (url: string, filePath: string) => {
-  const response = await fetch(url);
-  if (!response.ok) throw Error("http status error");
-
-  const array = await response.arrayBuffer();
-  const byteArray = new Uint8Array(array);
-  await Bun.write(filePath, byteArray);
-  console.log("write image", filePath);
-}
 
 async function main() {
   const browser = await prompt("type your browser", "chromium, brave-browser-stable, google-chrome-stable...") || "chromium";
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,26 @@
+export const prompt = async (message: string, example?: string) => {
+  process.stdout.write(`${message}\n`);
+  if (example) {
+    process.stdout.write(`(${example})\n`);
+  }
+  process.stdout.write('-> ');
+  for await (const line of console) {
+    process.stdout.write('\n');
+    return line;
+  }
+  return;
+}
+
+export const writeImageFile = async (url: string, filePath: string) => {
+  const response = await fetch(url);
+  if (!response.ok) throw Error("http status error");
+
+  const array = await response.arrayBuffer();
+  const byteArray = new Uint8Array(array);
+  await Bun.write(filePath, byteArray);
+  console.log("write image", filePath);
+}
+
 export const desktopFile = (browser: string, name: string, icon: string, url: string) => {
   return `[Desktop Entry]
 Type=Application
